7605ad9b61f7423ecf3da8c5face920113152afb
Brian Aguilar <anjacular@gmail.com>
2018-06-05 13:43:43 -0700

    Update OracleGenerics.java

--- OracleGenerics.java
+++ OracleGenerics.java
@@ -3,7 +3,7 @@ class OracleGenerics {
   #Swaps 2 elements in an array with the use of generics
   public static <T> void swapElements(T [] list,int pos1,int pos2){
     if(pos1<0 || pos1>list.length-1)throw new IllegalArgumentException("Invalid Positions"); 
-     if(pos2<0 || pos2>list.length-1)throw new IllegalArgumentException("Invalid Positions"); 
+    if(pos2<0 || pos2>list.length-1)throw new IllegalArgumentException("Invalid Positions"); 
     T temp = list[pos1];
     list[pos1] = list[pos2];
     list[pos2]=temp;
